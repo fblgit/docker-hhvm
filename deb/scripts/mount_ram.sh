0bb23e503da07245f49f0d7241c34832f08907e4
FBLGit <fblgit@users.noreply.github.com>
2016-04-21 11:48:28 +0800

    Update mount_ram.sh

--- deb/scripts/mount_ram.sh
+++ deb/scripts/mount_ram.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-apt-get install rsync -qy
-rsync -avz -q /mnt/cache/ /var/www/
-echo " Ramdisk Sync"
-if [[ -d "/mnt/fpc_cluster" ]]; then
-  if [[ -d "/mnt/fpc" ]]; then
-    rsync -avz -q --delete /mnt/fpc_cluster/ /mnt/fpc/
-    echo "FPC Sync"
+ENABLED_RAMDISK=${ENABLED_RAMDISK:-1}
+
+if [[ "$ENABLED_RAMDISK" == "1" ]]; then
+  apt-get install rsync -qy
+  rsync -avz -q /mnt/cache/ /var/www/
+  echo " Ramdisk Sync"
+  if [[ -d "/mnt/fpc_cluster" ]]; then
+    if [[ -d "/mnt/fpc" ]]; then
+      rsync -avz -q --delete /mnt/fpc_cluster/ /mnt/fpc/
+      echo "FPC Sync"
+    fi
   fi
 fi
