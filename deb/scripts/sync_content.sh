d77063e77863211973dccbbf4e65085c7ad041e9
FBLGit <fblgit@users.noreply.github.com>
2016-04-21 12:35:23 +0800

    Update sync_content.sh

--- deb/scripts/sync_content.sh
+++ deb/scripts/sync_content.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+## The Purpose is Execute a few Syncs, fully randomly in time.
+## Creates a non concurrent large scale event model
+## I suppose you dont want to run this at the same time in 500 containers.
 SYNC_ENABLED=${SYNC_ENABLED:-1}
+## If Sync is Disabled then it will only sleep
 # Major Sync Invoke every...
 REPEAT_CYCLE=18
 # Wait per each Cycle
 WAIT_CYCLE=1800
+# LOG
+LOG_FILE=/mnt/logs/content_sync.log
 
 # Define Major Sync Actions (heavy)
 function major_sync {
   rsync -avz --delete -q /mnt/cache/ /var/www/
-  # logger
+  echo "`date` $HOSTNAME >> HEAVY SYNC [OK]">>$LOG_FILE
 }
 # Define Minor Sync Actions (light)
 function minor_sync {
   rsync -avz -q /mnt/fpc/ /mnt/fpc_cluster/
   rsync -avz -q --delete /mnt/fpc_cluster/ /mnt/fpc/
+  echo "`date` $HOSTNAME >> LIGHT SYNC [OK]">>$LOG_FILE
 }
 # On All Sync
 function all_sync {
@@ -29,17 +36,20 @@ if [[ "$1" == "NOW_DAEMON" ]]; then
 fi
 CYCLE=0
 RANDOM=$(((RANDOM%120)))
+
+while sleep 0; do
+RANDOM=$(((RANDOM%180)))
+# Ensure in large scale its not concurrent... 
+sleep $RANDOM
 if [[ "$SYNC_ENABLED" == "1" ]]; then
-  while sleep 0; do
-  RANDOM=$(((RANDOM%180)))
-  # Ensure in large scale its not concurrent... 
-  sleep $RANDOM
   if [[ "$CYCLE" == "$REPEAT_CYCLE" ]]; then
     # Major Sync
     major_sync
     CYCLE=0
   fi
+  sleep $RANDOM
   minor_sync
-  CYCLE=$(($CYCLE+1))
-  sleep $WAIT_CYCLE
+fi
+CYCLE=$(($CYCLE+1))
+sleep $WAIT_CYCLE
 fi
